aa9451f4989e67c7db66bb2112458fabbd407f33
MartinDkov06 <dal240598@spengergasse.at>
2025-09-28 23:13:16 +0200

    hü

---
+++ 2025-09_dalakov_28_09/Student.java
@@ -0,0 +1,129 @@
+public class Student
+{
+    private String name;
+    private float kg;
+    private int cm;
+    private char gender;
+
+    // Constructor
+    public Student (String name, float kg, int cm, char gender) {
+        if (kg >625) {
+        throw new IllegalArgumentException("Gewicht muss kleiner als 625kg sein!");
+        
+    } else if (kg <0) {
+    throw new IllegalArgumentException("Gewicht muss größer als 0 sein!");
+    }
+    if (cm >272) {
+        throw new IllegalArgumentException("Größe muss kleiner als 272cm sein!");
+        
+    } else if (cm <30) {
+    throw new IllegalArgumentException("Größe muss größer als 30cm sein!");
+    
+    }
+    if (name.length()<3 || name.length()>50 ) {
+        throw new IllegalArgumentException("Name muss mehr als drei Buchstaben haben!");
+    
+        
+    }
+    
+        
+        this.setName(name);
+        this.setKg(kg);
+        this.setCm(cm);
+        this.setGender(gender);
+    }
+    // setter für name
+    public void setName (String name) {
+        this.name = name;
+    }
+
+    // setter für kg
+    public void setKg (float kilogramm) {
+        this.kg = kilogramm;
+    }
+
+    // setter für cm
+    public void setCm (int cm) {
+        this.cm = cm;
+    }
+
+    // calculate bmi
+    public float bmi () {
+        return this.kg/((this.cm/100.0f)*(this.cm/100.0f));
+    }
+
+    public String mannOderFrau () {
+        if (this.gender == 'm') return "männlich";
+        // this.
+        return "weiblich";
+    }
+
+    public String toString () {
+        return "Name: " + this.name + " (" + this.mannOderFrau() + ")" + mannOderFrau();
+    }
+
+    public void setGender (char gender) { //String = Alles in den grünen anführungszeichen (oft Buchstaben)
+        gender = Character.toLowerCase(gender);
+        if(gender !='m' && gender !='w') {
+            throw new IllegalArgumentException ("Geschlecht darf nur m oder w sein!");
+            
+        } else
+        
+        
+        
+        
+        this.gender = gender;
+    }
+
+    public String bmiBewerter () {
+        if (this.gender == 'm') {
+            if (bmi() < 20) {
+                return "untergewicht";
+
+            } else if (bmi() > 25) {
+                return "übergewicht";
+
+            
+            } else {
+                return "normalgewicht";
+
+            }
+
+        }
+        if (this.gender == 'w') {
+            if (bmi() < 19) {
+                return "untergewicht";
+
+            } else if (bmi() > 24) {
+                return "übergewicht";
+
+            
+            } else {
+                return "normalgewicht";
+
+            }
+
+        }
+        return "GondiBalsam";
+    }
+    
+    public String getName()
+    {
+        return name;
+    }
+    
+    public float getKg()
+    {
+        return kg;
+    }
+    
+    public int getCm() {
+    return cm;
+    }
+    
+    public char getGender() {
+    return gender;
+    }
+    
+}
+
---
+++ 2025-09_dalakov_28_09/StudentTest.java
@@ -0,0 +1,176 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import static org.junit.Assert.assertEquals;
+
+/**
+ * The test class StudentTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class StudentTest
+{
+    /**
+     * Default constructor for test class StudentTest
+     */
+    public StudentTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    
+    @Test
+    public void testSetName()
+    {
+        // String name, float kg, int cm, char gender
+        Student s = new Student("Dalakov", 89.0f, 190, 'm');
+        assertEquals("Dalakov", s.getName());
+        s.setName("Graf");
+        assertEquals("Graf", s.getName());
+        
+    }
+    
+    @Test
+    public void testSetKg()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 89.0f, 190, 'm');
+        assertEquals(89.0f, f.getKg(),0.1);
+        f.setKg(79.0f);
+        assertEquals(79.0f, f.getKg(), 0.1);
+        
+    }
+    
+    @Test
+    public void testSetCm()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 89.0f, 190, 'm');
+        assertEquals(190, f.getCm());
+        f.setCm(180);
+        assertEquals(180, f.getCm());
+        
+    }
+    
+    @Test
+    public void testSetGender()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 89.0f, 190, 'm');
+        assertEquals('m', f.getGender());
+        f.setGender('w');
+        assertEquals('w', f.getGender());
+        
+    }
+    
+    
+    //Aufgabe vom 25.09.2025
+    @Test
+    public void testBmiBewerter_mann_normalgewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 89.0f, 190, 'm');
+        assertEquals("normalgewicht", f.bmiBewerter());
+        
+    }
+    @Test
+    public void testBmiBewerter_mann_übergewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 120.0f, 190, 'm');
+        assertEquals("übergewicht", f.bmiBewerter());
+        
+    }
+    @Test
+    public void testBmiBewerter_mann_untergewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 50.0f, 190, 'm');
+        assertEquals("untergewicht", f.bmiBewerter());
+        
+    }
+    @Test
+    public void testBmiBewerter_frau_normalgewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Rosa", 50.0f, 155, 'w');
+        assertEquals("normalgewicht", f.bmiBewerter());
+        
+    }
+    @Test
+    public void testBmiBewerter_frau_übergewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Rosa", 89.0f, 167, 'w');
+        assertEquals("übergewicht", f.bmiBewerter());
+        
+    }
+    @Test
+    public void testBmiBewerter_frau_untergewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 35.0f, 165, 'w');
+        assertEquals("untergewicht", f.bmiBewerter());
+        
+    }
+    @Test
+    public void testBmi_dalakov_untergewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 35.0f, 165, 'm');
+        assertEquals(12.9f,f.bmi(),0.1f);
+        
+    }
+    @Test
+    public void testBmi_dalakov_normalgewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 75.0f, 185, 'm');
+        assertEquals(21.9f,f.bmi(),0.1f);
+        
+    }
+    @Test
+    public void testBmi_dalakov_übergewichtig()
+    {
+        // String name, float kg, int cm, char gender
+        Student f = new Student("Dalakov", 120.0f, 175, 'm');
+        assertEquals(39.2f,f.bmi(),0.1f);
+        
+    }
+    @Test
+    public void falschesGeschlecht() {
+        assertThrows(IllegalArgumentException.class, () -> {
+            new Student("Rosa", 60.0f, 170, 'x');
+        });
+    }
+}
+    
+    
+  
+
+
+
+
+
