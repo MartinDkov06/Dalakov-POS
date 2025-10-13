e55ce3583e10d8aeb1df4a1b7d5fe0f94fd17b6e
MartinDkov06 <dal240598@spengergasse.at>
2025-10-13 17:10:32 +0200

    HÜ

---
+++ HTL/SameEqual.java
@@ -0,0 +1,17 @@
+
+
+public class SameEqual
+{
+  public boolean equals (Object other) 
+  {
+  return false;  
+    }
+    
+    public static void demoEq ()
+    {
+    SameEqual first = new SameEqual();
+    
+    
+    
+    }
+}
